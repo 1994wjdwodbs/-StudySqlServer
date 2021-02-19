73a1ff7428150d19cb29ddd5907b665dace06743
JYJ_FROM_KOOKBI <1994wjdwodbs@gmail.com>
2021-02-19 11:48:17 +0900

    JYJ SQL PRACTICE

--- 17_test/20210218_정재윤_시험쿼리.sql
+++ 17_test/20210218_정재윤_시험쿼리.sql
@@ -28,7 +28,7 @@ FROM bookstbl
 ORDER BY Idx DESC;
 
 
--- 3-1
+-- 3-1 
 SELECT 
 	b.Idx AS 번호
 	, b.Division AS 장르번호
@@ -38,8 +38,35 @@ SELECT
 FROM bookstbl AS b
 JOIN divtbl AS d on (b.Division = d.Division)
 WHERE b.Division = 'B002';
+-- 3-1 다른 풀이 (용어 생략)
+SELECT 
+	b.Idx 번호 -- AS 생략해도 됨
+	, b.Division '장르 번호' -- 띄어쓰기
+	, d.Names 장르
+	, b.Names 책제목
+	, b.Author 저자
+FROM bookstbl b
+JOIN divtbl d on (b.Division = d.Division)
+WHERE b.Division = 'B002';
+-- 3-1 다른 풀이 (서브쿼리)
+SELECT 
+	b.Idx 번호
+	, b.Division '장르 번호'
+	, (SELECT Names FROM divtbl WHERE Division = b.Division) 장르
+	, b.Names 책제목
+	, b.Author 저자
+FROM bookstbl b
+WHERE b.Division = 'B002';
 
 -- 3-2
+SELECT
+	m.Names
+	, m.Levels
+	, m.Addr
+	, NULL rentalDate
+FROM membertbl AS m
+WHERE m.idx NOT IN (SELECT memberIdx FROM rentaltbl AS r);
+-- 3-2 다른 풀이 (서브쿼리)
 SELECT
 	m.Names
 	, m.Levels
@@ -67,3 +94,15 @@ SELECT
 FROM bookstbl AS b
 JOIN divtbl AS d ON (b.Division = d.Division)
 GROUP BY ROLLUP(d.Names);
+-- 5 다른 풀이
+SELECT 
+	(SELECT names FROM divtbl WHERE Division = a.Division) names
+	, a.sum_price 총합계금액
+	FROM (
+			SELECT b.Division, SUM(b.price) sum_price
+			FROM bookstbl b
+			GROUP BY b.Division
+		 ) a
+UNION ALL
+SELECT null, SUM(price)
+FROM bookstbl;
